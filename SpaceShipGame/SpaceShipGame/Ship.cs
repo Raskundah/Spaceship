b38a307a878b1567ea4173ad69cb428710f2bd4f
Raskundah <100294321+Raskundah@users.noreply.github.com>
2022-03-24 15:27:52 +0000

    0.1
    Ship class created, content loaded, sprites drawn. screen size initialized.

---
+++ SpaceShipGame/SpaceShipGame/Ship.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace SpaceShipGame
+{
+    class Ship
+    {
+        public Vector2 position = new Vector2(100, 100);
+    }
+}
