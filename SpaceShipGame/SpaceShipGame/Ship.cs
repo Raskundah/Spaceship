917e4fd9b030ee727948f57e4bc4a1f2a2abf2ed
Raskundah <100294321+Raskundah@users.noreply.github.com>
2022-03-24 15:57:33 +0000

    Update Ship.cs
    movement code

--- SpaceShipGame/SpaceShipGame/Ship.cs
+++ SpaceShipGame/SpaceShipGame/Ship.cs
@@ -13,7 +13,27 @@ namespace SpaceShipGame
 
         public void ShipUpdate()
         {
-            position.Y++;
+            KeyboardState kState = Keyboard.GetState();
+
+            if (kState.IsKeyDown(Keys.A) || kState.IsKeyDown(Keys.Left))
+            {
+                position.X--;
+            }
+
+            if (kState.IsKeyDown(Keys.D) || kState.IsKeyDown(Keys.Right))
+            {
+                position.X++;
+            }
+
+            if (kState.IsKeyDown(Keys.S) || kState.IsKeyDown(Keys.Down))
+            {
+                position.Y++;
+            }
+
+            if (kState.IsKeyDown(Keys.W) || kState.IsKeyDown(Keys.Up))
+            {
+                position.Y--;
+            }
         }
     }
 }
